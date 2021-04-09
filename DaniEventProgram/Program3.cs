9958f03052f20c9d70a4f8d5eca20823f1da112b
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-04-09 16:01:07 +0200

    changed

--- DaniEventProgram/Program3.cs
+++ DaniEventProgram/Program3.cs
@@ -30,7 +30,7 @@ namespace DaniEventProgram
             if (number == 2) return true;
             if (number % 2 == 0) return false;
 
-            var boundary = (int)Math.Floor(Math.Sqrt(number));
+            var boundary = (int)Math.Sqrt(number);
 
             for (int i = 3; i <= boundary; i += 2)
                 if (number % i == 0)
